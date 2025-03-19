496ad77d729a775c87000948d9d88637e32aca9a
Waqas-Tanole <yalgharssgarmy@gmail.com>
2025-03-19 14:19:40 +0500

    All Schemas for login, signup, verifiy OTP, accept Message, Message Schema done

---
+++ src/app/schemas/signUpSchema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+export const usernameValidation = z
+  .string()
+  .min(2, "user name must be atleat 2 characters")
+  .max(20, "usrname must not be longer than 20 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "User name must not contain special characters.");
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid Email!" }),
+  passsword: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
