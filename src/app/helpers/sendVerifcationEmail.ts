d56d5ddde1282ef2f2046ccc6215ed41d614a392
Waqas-Tanole <yalgharssgarmy@gmail.com>
2025-03-19 17:35:27 +0500

    Send Email OTP Setup completed, Sign up set up completed, Complete sign up logic completed along with email verification

---
+++ src/app/helpers/sendVerifcationEmail.ts
@@ -0,0 +1,22 @@
+import { resend } from "../lib/resend";
+import verificationEmail from "../../../emails/VerificationEmail";
+import { ApiResponse } from "../types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Mystry Message | Verification Code",
+      react: verificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: "Verification email sent successfully!" };
+  } catch (emailError) {
+    console.log("Error sending Verification email", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
+}
